740936d0572216a26bc9ef66b81d13335524eb68
GiulioRossetti <giulio.rossetti@gmail.com>
2024-07-21 18:41:54 +0200

    :new: reorganization experimental settings

--- y_server/__init__.py
+++ y_server/__init__.py
@@ -1,9 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import json
+import shutil
+import os
+
+# create the experiments folder
+if not os.path.exists('./experiments'):
+    os.mkdir('./experiments')
+
+# read the experiment configuration
+config = json.load(open("config_files/exp_config.json"))
+
+# copy the clean database to the experiments folder
+shutil.copyfile("data_schema/database_clean_server.db", f"experiments/{config['name']}.db")
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "4YrzfpQ4kGXjuP6w"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///../experiments/{config['name']}.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
